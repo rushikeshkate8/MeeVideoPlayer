69bd61eaeaa2cbe1951cc2b0339dd545f39a02d2
Rushikesh Kate <rushikeshkate8@gmail.com>
2021-05-24 12:34:52 +0530

    Video Item added

--- app/src/main/java/com/mee/main/MainActivity.java
+++ app/src/main/java/com/mee/main/MainActivity.java
@@ -3,9 +3,12 @@ package com.mee.main;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.ismaeldivita.chipnavigation.ChipNavigationBar;
 import com.mee.player.R;
 import com.mee.player.databinding.ActivityMainBinding;
+import com.mee.player.databinding.VideoItemBinding;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -15,5 +18,23 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate( savedInstanceState );
         binding = ActivityMainBinding.inflate( getLayoutInflater() );
         setContentView( binding.getRoot() );
+        setUpBottomNavigationBar();
+    }
+
+    void setUpBottomNavigationBar() {
+        binding.bottomNavBar.setOnItemSelectedListener( new ChipNavigationBar.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(int i) {
+                switch (i) {
+                    case R.id.folders_menu_item:
+                        Toast.makeText( MainActivity.this , "Folders Item clicked" , Toast.LENGTH_SHORT ).show();
+                        break;
+                    case R.id.videos_menu_item:
+                        Toast.makeText( MainActivity.this , "Videos Item clicked" , Toast.LENGTH_SHORT ).show();
+                        break;
+                }
+            }
+        });
     }
 }
