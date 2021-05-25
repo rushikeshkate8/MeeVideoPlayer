064af0f3cceb0d86199f3b92fab62e79a8eb4176
Rushikesh Kate <rushikeshkate8@gmail.com>
2021-05-25 20:29:09 +0530

    Player Design Modified

--- app/src/main/java/com/mee/player/Utils.java
+++ app/src/main/java/com/mee/player/Utils.java
@@ -14,6 +14,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
+import android.widget.RelativeLayout;
 
 import com.google.android.exoplayer2.Format;
 
