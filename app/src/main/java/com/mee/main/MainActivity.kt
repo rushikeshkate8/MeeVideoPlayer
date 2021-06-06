63eacdd81cbda018f4f21ca201bbd38887e89429
Rushikesh Kate <rushikeshkate8@gmail.com>
2021-06-06 21:43:12 +0530

    Splash Screen added

--- app/src/main/java/com/mee/main/MainActivity.kt
+++ app/src/main/java/com/mee/main/MainActivity.kt
@@ -59,6 +59,8 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
 
         setUpBottomNavigationBar()
 
+        Toast.makeText(this, "Developed by Rushikesh Kate", Toast.LENGTH_LONG).show()
+
         //setUpObservers()
     }
 
